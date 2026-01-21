9c903c8ecc7441a5ecb99bad75ca37c00a7538b3
Seindi Rahmat Barus <xseindi@gmail.com>
2026-01-21 12:50:03 +0700

    .

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ var express = require ("express.io.js")
 $.config = require ($.path.r ("package.json")).config
 const {define} = $
 
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 var app = new express ($.config)
 
 app.static ()
@@ -22,38 +32,58 @@ app.setup (function (request, response, next) {
 	next ()
 	})
 
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 app.get ("/", function (request, response) {
 	console.log ($.path.join (process.cwd (), "api/test.js"))
 	response.html (response.output.render ())
 	})
 
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 app.get ("/robots.txt", function (request, response) {
 	response.html ("xxx")
 	})
 
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 app.catch (function (request, response) {
 	response.status ($.url.header.status.error.found).html (response.output.render ())
 	})
-
-/**
- * xxx
- *
- * title
- * description
- * sub description
- *
- * xxx://xxx.xxx.xxx/xxx
- */
-
-/**
- * xxx
- *
- * title
- * description
- * sub description
- *
- * xxx://xxx.xxx.xxx/xxx
- */
 
 /**
  * xxx
