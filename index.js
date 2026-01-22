e5a8d2692cf829e5f5dfa64d9c876d6513bda3b4
Seindi Rahmat Barus <xseindi@gmail.com>
2026-01-22 10:53:59 +0700

    .

--- index.js
+++ index.js
@@ -17,15 +17,13 @@ var $ = require ("script.io.js")
 var express = require ("express.io.js")
 const {define} = $
 var __dir = process [["C", "W", "D"].join ("").small ()] ()
-var {fileURLToPath} = require ("url")
-__dir = "."
 
 var j_son = {
-	app: require ($.path.join (__dir, "src/db/app.json")),
-	theme: require ($.path.join (__dir, "src/db/theme.json")),
+	app: require ("./src/db/app.json"),
+	theme: require ("./src/db/theme.json"),
 	}
 
-express.config = require ($.path.join (__dir, "package.json")).config
+express.config = require ("./package.json").config
 express.config.dir = (__dir)
 express.config.db.collection = j_son
 
