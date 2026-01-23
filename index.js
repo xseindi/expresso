9d409aef2da1b08fd78f1d5e4431b3c2a592e2d5
Seindi Rahmat Barus <xseindi@gmail.com>
2026-01-23 10:04:47 +0700

    .

--- index.js
+++ index.js
@@ -8,27 +8,18 @@
  * xxx://xxx.xxx.xxx/xxx
  */
 
-/*
-1024
-1048576
-*/
-
 var $ = require ("script.io.js")
 var express = require ("express.io.js")
 const {define} = $
 var __dir = process [["C", "W", "D"].join ("").small ()] ()
 
-console.log ($.unique.id ())
-console.log ($.unique.id ())
-console.log ($.unique.id ())
-
 express.config = require ($.path.current ("package.json")).config
 express.config.dir = (__dir)
 if (true) express.config.db.json = {
-	account: require ($.path.current ("src/db/account.json")),
-	app: require ($.path.current ("src/db/app.json")),
-	instance: require ($.path.current ("src/db/instance.json")),
-	theme: require ($.path.current ("src/db/theme.json")),
+	account: require (("./src/db/account.json")),
+	app: require (("./src/db/app.json")),
+	instance: require (("./src/db/instance.json")),
+	theme: require (("./src/db/theme.json")),
 	}
 
 /**
